d3227b0d271c8cbbbb840f4120705b729b3bf3f9
Charlie-Brownie85 <carlosorbea85@gmail.com>
2024-03-22 10:32:25 +0100

    chore: initial commit

---
+++ src/components/Game/GameCompo.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+export interface Props {
+  configuration: Array<number>,
+  onSolved?: () => void,
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <div />
+</template>
+
+<style lang="postcss" scoped>
+
+</style>
